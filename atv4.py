11b223f0b7afd7016051966b894cf6d3406ca4b8
sofymiranda <mirandaspfya@gmail.com>
2023-10-11 11:12:46 -0300

    atividade4

---
+++ atv4.py
@@ -0,0 +1,6 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+print ("descubra o antecessor e o sucessor de um número qualquer")
+numero = float (input("digite o número: "))
+sucessor= numero + 1
+antecessor= numero - 1
+print( f"sucessor do número é {sucessor} e o antecessor é {antecessor}")
